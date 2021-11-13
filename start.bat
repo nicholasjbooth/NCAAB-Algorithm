fbdaab13f2eabf79922fbe27cb38049df0558cd3
Nick <nickbooth20@outlook.com>
2021-11-13 17:05:54 -0500

    ?

--- start.bat
+++ start.bat
@@ -1,2 +1,3 @@
 python3 updateKP.py && cd ncaab-backend && python3 server.py &  cd ncaab-frontend && npm run start
 pause
+
---
+++ start2.bat
@@ -0,0 +1,2 @@
+start cmd /k "python3 updateKP.py && cd ncaab-backend && python3 server.py"
+start cmd /k "cd ncaab-frontend && npm run start"
